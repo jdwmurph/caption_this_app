e910c92c3e5567c5cb2c0019822864af63599763
jdwmurph <jdwmurph@gmail.com>
2014-05-18 22:56:54 -0400

    Fixed seeding issue on local host. Should be final commit before moving forward

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,5 @@ class WelcomeController < ApplicationController
     @captions = @image.captions
     @captions_sorted = @captions.sort {|caption_2, caption_1| caption_1.get_upvotes.size <=> caption_2.get_upvotes.size}
     @caption_top = @captions_sorted.first
-    end
   end
-
 end
