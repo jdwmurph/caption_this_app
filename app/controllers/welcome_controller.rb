84557bbf22f25717da5c97b4b131350f58992cce
jdwmurph <jdwmurph@gmail.com>
2014-05-18 22:41:47 -0400

    added if statement to welcome controller to enable heroku deployement if nil class. this is a test. may not work.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,12 @@ class WelcomeController < ApplicationController
   def index
     @image = Image.first
     @captions = @image.captions
-    @captions_sorted = @captions.sort {|caption_2, caption_1| caption_1.get_upvotes.size <=> caption_2.get_upvotes.size}
-    @caption_top = @captions_sorted.first
+    if @captions
+       @captions_sorted = @captions.sort {|caption_2, caption_1| caption_1.get_upvotes.size <=> caption_2.get_upvotes.size}
+    end
+    if @captions_sorted
+      @caption_top = @captions_sorted.first
+    end
   end
 
 end
